66fcff34577b6ffb3ec2336d1e9136b4a6998f4a
Paul <quaifep@yahoo.com>
2021-01-13 16:06:32 -0800

    Initial commit

---
+++ change.py
@@ -0,0 +1,14 @@
+# Author: Paul Quaife
+# Date: 1/12/2020
+# Description: Gives change for less than a dollar in highest amount.
+
+print("Please enter an amount in cents less than a dollar.")
+coins = float(input())
+print("Your change will be:")
+print("Q:", coins // 25)
+coins = coins % 25
+print("D:", coins // 10)
+coins = coins % 10
+print("N:", coins // 5)
+coins = coins % 5
+print("P:", coins)
